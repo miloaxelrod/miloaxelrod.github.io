5ac4944a1d9254cf0819a17b0897a324f2190e20
miloaxelrod <miloaxelrod@gmail.com>
2017-09-25 17:44:45 -0400

    fix thing

--- homework2.js
+++ homework2.js
@@ -5,7 +5,7 @@ var chosen = prompt("What's your favorite poodle mix?");
 
 alert("Your favorite breed is " + chosen + "? Mine is " + fave + "!")
 
-if (chosen === "Schnoodle" || "schnoodle") {
+if (chosen === "Schnoodle") {
   alert("You're a superior human who loves superior dogs! I have declared it so.");
 } else {
   alert("You'll never know the TRUE best poodle mix. You'll die a plebian.");
